3796b323c3e548d84127c785f08600a96f6963f5
Shahjalal Hossain <shossainfolk@gmail.com>
2017-08-13 22:17:18 +0600

    updated App.jsx

---
+++ client/app/components/App.jsx
@@ -0,0 +1,7 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from '.home/Home';
+
+const contentNode = document.getElementById('container');
