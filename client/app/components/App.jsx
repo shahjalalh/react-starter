7963e2bedb44df644b3c87d7c89aad52445b85a4
Shahjalal Hossain <shossainfolk@gmail.com>
2017-08-14 01:16:31 +0600

    updated App and Home for starter

--- client/app/components/App.jsx
+++ client/app/components/App.jsx
@@ -2,6 +2,7 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Home from '.home/Home';
+import Home from '.home/Home.jsx';
 
 const contentNode = document.getElementById('container');
+ReactDOM.render(<Home />, contentNode);
