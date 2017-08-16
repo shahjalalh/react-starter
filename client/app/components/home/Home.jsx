2523696940b4f727dfabdf072a47f86e6da64f0e
Shahjalal Hossain <shossainfolk@gmail.com>
2017-08-16 14:32:48 +0600

    added react-icons

--- client/app/components/home/Home.jsx
+++ client/app/components/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './home.scss';
+import FaBeer from 'react-icons/fa/beer';
 
 export default class Home extends React.Component{
     render(){
         return (
             <div>
                 <h1 className='home'>Hello react starter</h1>
+                <h3>Lets have some <FaBeer /></h3>
             </div>
         );
     }
